e1309c0fabac64395aefc741c8bbacf82e4bab35
KESHAV4820 <keshav48kumar@gmail.com>
2024-08-19 23:53:46 +0530

    after establishing connection with the remote database

--- 4-natours/after-section-06/server.js
+++ 4-natours/after-section-06/server.js
@@ -4,9 +4,17 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+// to connect to the remotely hosted database. 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-mongoose.connect(DB, {useNewUrlParser:true, useCreateIndex: true, useFindAndModify: false}).then(returnedConnectionObject => {
-  console.log(returnedConnectionObject.connections);
+mongoose.connect(DB, {
+
+// mongoose.connect(process.env.DATABASE_LOCAL,{  //Remember It we use this syntex if we are connecting to local hosted database. Only This much changes.😎 
+      useNewUrlParser:true, 
+      useCreateIndex: true, 
+      useFindAndModify: false
+}// is recipe! not concept
+).then(returnedConnectionObject => {
+  console.log(returnedConnectionObject.connections);// Code Testing
   console.log("DB connection successful!!");
   
 });
